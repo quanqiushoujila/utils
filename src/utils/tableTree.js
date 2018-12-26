27d232369b10ea0bf91f39911cf5d4230cee70f5
k <k@k.com>
2018-12-26 17:27:21 +0800

    tabletree

--- src/utils/tableTree.js
+++ src/utils/tableTree.js
@@ -1,3 +1,9 @@
-export function setTableTreeData () {
+export function setTableTreeData (data = [], {expanded = '_expaneded', level = '_level', show = '_show', children = 'children'}) {
+  for (let i = 0, i < data,length; i < len; i++) {
+
+  }
+}
+
+function loop (data) {
 
 }
