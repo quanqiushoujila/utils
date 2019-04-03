0fe1aef913cb3e6f0c0ed6d42a1e93f2cce290f4
邵琦凯 <k@k.com>
2019-04-03 17:05:13 +0800

    formMaking

---
+++ src/components/FormMaking/demo/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <fm-making-form ref="makingForm" upload preview generate-code generate-json v-model="data">
+    <template slot="action">
+    </template>
+  </fm-making-form>
+</template>
+
+<script>
+export default {
+  mounted () {
+  },
+  data () {
+    return {
+      type: {
+        textarea: 'textarea',
+        input: 'input',
+        time: 'time',
+        date: 'date'
+      },
+      data: [
+        {
+          type: 'input',
+          name: '输入框'
+        },
+        {
+          type: 'textarea',
+          name: '大输入框'
+        }
+      ]
+    }
+  }
+}
+</script>
